75bb3194a7a7c0ce34f2860879e1b308c340fdbf
Vasek-Doskar <doskar@spsjc.cz>
2024-05-02 11:25:56 +0200

    Přidejte soubory projektu.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Xml.Serialization;
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool Stav { get; set; }
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            DataContext = this;
+
+            this.Closing += (s, e) => Serializuj();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   // dynamické zjištění cesty k souboru
+            string cesta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FILE.txt");
+            if (!File.Exists(cesta))
+            {
+                MessageBox.Show("Soubor neexistuje");
+                return;
+            }
+
+            //Každá práce se souborem MUSÍ probíhat v bloku using nebo try-catch-finally
+            using (StreamWriter sw = new StreamWriter(cesta, true))
+            {
+                string obsah = Obsah.Text;
+                sw.WriteLine(obsah);
+                sw.Flush();
+                sw.Close();
+            }
+            Obsah.Text = string.Empty;
+            Kontrolka.Foreground = new SolidColorBrush(Colors.Green);
+        }
+
+        private void Obsah_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            Kontrolka.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+
+        private void Serializuj()
+        {
+            Settings set = new();
+            set.State = Stav;
+
+            XmlSerializer writer = new(typeof(Settings));
+            string CestaNastaveni = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "wpfinit.txt");
+            using (FileStream file = File.Create(CestaNastaveni))
+            {
+                writer.Serialize(file, set);
+                file.Close();
+            }
+        }
+
+        private void Deserializuj()
+        {
+            XmlSerializer reader = new(typeof(Settings));
+        }
+    }
+}
---
+++ WpfApp4/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    public class Settings
+    {
+        public bool State { get; set; }
+    }
+}
